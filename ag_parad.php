faf6541271f3f9b3f44b1758aaec5e52f11fe72c
JARG081 <121974065+JARG081@users.noreply.github.com>
2023-02-08 00:37:01 -0500

    Update ag_parad.php

--- ag_parad.php
+++ ag_parad.php
@@ -13,13 +13,14 @@ $query_2 =pg_query($sql2);
 <!DOCTYPE html>
 <html lang="es"></html>
 <head>
+    <link rel="shortcut icon" type="image/x-icon" sizes="128x128" href="img/favicon.png">
     <title>EDITAR RUTA</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://kit.fontawesome.com/ccd4ed56f8.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/estilos.css">
-    <link rel="shortcut icon" type="image/x-icon" sizes="128x128" href="img/favicon.png">
+    
     
 </head>
 <body>
